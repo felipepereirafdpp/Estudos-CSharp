55d82aa9f51103888fc0bc25ae4917f8e91c33f0
felipepereirafdpp <felipepereira.fdpp@gmail.com>
2026-07-15 02:31:21 -0300

    remove : removido exercicio 34

---
+++ Beecrowd/Exercicio-34/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+int M = int.Parse(Console.ReadLine());
+int N = int.Parse(Console.ReadLine());
+
+int[,] matriz = new int[M, N];
+
+for (int i = 0; i < M; i++)
+{
+    string[] vetor = Console.ReadLine().Split(" ");
+    for (int j = 0; j < M; j++)
+    {
+        matriz[i, j] = int.Parse(vetor[j]);
+    }
+}
+for (int i = 0; i < M; i++)
+{
+    for (int j = 0; j < N; j++)
+    {
+        Console.Write(matriz[i, j]+ " ");
+    }
+    Console.WriteLine();
+}
