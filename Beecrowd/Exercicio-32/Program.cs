a98815fd1ec629e49296eb791bcea3ee2ac78a49
felipepereirafdpp <felipepereira.fdpp@gmail.com>
2026-07-15 01:23:19 -0300

    feat: adicionado exercicio 32

---
+++ Beecrowd/Exercicio-32/Program.cs
@@ -0,0 +1,41 @@
+﻿int M, N;
+
+string[] vetor = Console.ReadLine().Split(" ");
+M = int.Parse(vetor[0]);
+N = int.Parse(vetor[1]);
+int[,] matrizA = new int[M,N];
+int[,] matrizB = new int[M, N];
+int[,] matrizC = new int[M, N];
+
+for (int i = 0; i < M; i++) 
+{
+    string[] vetA = Console.ReadLine().Split(" "); 
+    for (int j = 0; j < N; j++)
+    {
+        matrizA[i, j] = int.Parse(vetA[j]);
+    }
+}
+for (int i = 0; i < M; i++) 
+{
+    string[] vetB = Console.ReadLine().Split(" ");
+    for (int j = 0; j < N; j++)
+    {
+        matrizB[i, j] = int.Parse(vetB[j]);
+    }
+}
+for (int i = 0; i < M; i++) 
+{
+    for (int j = 0; j < N; j++)
+    {
+        matrizC[i, j] = matrizA[i, j] + matrizB[i, j];
+    }
+}
+
+for (int i = 0; i < M; i++)
+{
+    for (int j = 0; j < N; j++)
+    {
+        Console.Write(matrizC[i, j]+ " ");
+    }
+    Console.WriteLine();
+}
---
+++ Beecrowd/Exercicio-32/README.md
@@ -0,0 +1,27 @@
+# 📚 Desafio Beecrowd - 32
+
+**📅 Data:** 14/07/2026  
+**⏱️ Tempo do Desafio:** 8 Minutos
+
+## 📝 Resumo do Exercício
+
+> Desenvolvi um programa que lê duas matrizes de números inteiros com a mesma quantidade de linhas e colunas, calcula a soma dos elementos correspondentes e gera uma terceira matriz com os resultados, exibindo-a ao final.
+
+## 💡 Conceitos aprendidos
+
+* Leitura de dados.
+* Matrizes (`int[,]`).
+* Estrutura de repetição `for`.
+* Percorrer linhas e colunas de uma matriz.
+* Operações entre matrizes.
+* Soma de elementos correspondentes.
+* Armazenamento de resultados em uma terceira matriz.
+* Entrada e saída de dados (`Console.ReadLine` e `Console.WriteLine`).
+* Utilização do método `Split()` para separar os dados informados pelo usuário.
+
+---
+
+## 💻 Teve dificuldade
+
+* [] Sim
+* [x] Não
