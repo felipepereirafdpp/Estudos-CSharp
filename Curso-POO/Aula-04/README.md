def1680068f1cafe39249ac54c21e2ff439a5807
felipepereirafdpp <felipepereira.fdpp@gmail.com>
2026-08-18 15:25:12 -0300

    feat: adiciona aula 4 e 5

---
+++ Curso-POO/Aula-04/README.md
@@ -0,0 +1,28 @@
+# 📚 Aula 04 - POO
+
+**📅 Data:** 18/08/2026  
+**⏱️ Tempo de estudo:** 15 Minutos
+
+## 📝 Resumo da aula
+
+> Foi uma aula onde revi os conceitos sobre construtores e aprendi sobrecarga de construtores.
+
+---
+
+## 💡 Conceitos aprendidos
+
+
+
+* Métodos statics
+* `Construtores`
+* `Sobrecarga de construtores`
+* `CultureInfo.InvariantCulture`
+
+---
+
+## 💻 Exercícios realizados
+
+* [x] Ex01  
+ 
+
+---
---
+++ Curso-POO/Aula-05/Produto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+// Exemplos usando This
+
+namespace Aula_05
+{
+    public  class Produto
+    {
+        public string Nome { get; set; }
+        public string Observacao { get; set; }
+        public int QuantidadeEstoque { get; set; }
+
+        
+
+        // Geralmente os parametros sao com letra minuscula mas nesse exemplo os parametros sao com letra maisucula para usar o
+        // This para diferenciar o parametro do atributo para o sistema conseguir diferenciar
+        public Produto(string Nome , string Observacao)
+        {
+            this.Nome = Nome;
+            this.Observacao = Observacao;
+        }
+
+
+        // Chamando outro contrutor para esse usando this, ele nao herda automaticamente os 
+        // atributos do outro construtor tem que informar qual voce deseja e passa como parametros
+
+        public Produto (string nome , string observacao ,int quantidadeEstoque) : this(nome, observacao)
+        {
+            QuantidadeEstoque = quantidadeEstoque;
+        }
+        // Outra forma é na propi class chmar o propio method como argumento na intancia do objeto
+    }
+}
